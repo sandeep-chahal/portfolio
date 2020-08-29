83405d5fa8fbc8727edd85aec028e283965b2c1d
sandeep chahal <Sandeepchahal573@gmail.com>
2020-08-29 11:14:32 +0530

    created pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,12 @@
 import React from "react"
+import { Link } from "gatsby"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      Hello world!
+      <Link to="/Projects">Projects</Link>
+      <Link to="/Contact">Contact</Link>
+    </div>
+  )
 }
