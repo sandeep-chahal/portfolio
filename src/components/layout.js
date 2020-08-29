c8fc4641938c128e6ce06fabb95490ca49f53a4f
sandeep chahal <Sandeepchahal573@gmail.com>
2020-08-29 11:55:38 +0530

    added sass support added layout

---
+++ src/components/layout.js
@@ -0,0 +1,6 @@
+import React from "react"
+import "../styles/index.scss"
+
+export default ({ children }) => {
+  return <div className="layout">{children}</div>
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,13 @@
 import React from "react"
 import { Link } from "gatsby"
+import Layout from "../components/layout"
 
 export default function Home() {
   return (
-    <div>
+    <Layout>
       Hello world!
       <Link to="/Projects">Projects</Link>
       <Link to="/Contact">Contact</Link>
-    </div>
+    </Layout>
   )
 }
